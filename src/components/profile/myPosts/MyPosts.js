644f8b71083e217d9e66f94d23ef435516134848
AlbinaAlb <albinakovalevaa@gmail.com>
2022-01-14 13:58:06 +0200

    inital commit

--- src/components/profile/myPosts/MyPosts.js
+++ src/components/profile/myPosts/MyPosts.js
@@ -4,13 +4,14 @@ import Post from './post/Post'
 const MyPosts = () => {
   return (
     <div>
-        My posts
-        <div>
-          <textarea></textarea>
-          <button>Add post</button>
-        </div>
+      My posts
+      <div>
+        <textarea></textarea>
+        <button>Add post</button>
+      </div>
       <div className={s.posts}>
-        <Post />
+        <Post message="Hi, how are you?" like="15 likes" />
+        <Post message="It's my first post" like="20 likes" />
       </div>
     </div>
   )
--- src/components/profile/myPosts/post/Post.js
+++ src/components/profile/myPosts/post/Post.js
@@ -1,15 +1,15 @@
 import s from './Post.module.scss'
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.post}>
       <img
         src="https://i.pinimg.com/originals/f1/9c/f5/f19cf5aaa0ea38889c940c12d8ab41b9.jpg"
         alt=""
       />
-      post 1
+      {props.message}
       <div>
-        <span>like</span>
+        <span>{props.like}</span>
       </div>
     </div>
   )
