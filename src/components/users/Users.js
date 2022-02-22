20213e5f86cda356f0f8b07eecca850d5b010431
AlbinaAlb <albinakovalevaa@gmail.com>
2022-02-22 15:38:27 +0200

    UsersContainer - 56

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,112 +1,84 @@
-import React from 'react'
+import React from "react";
 import styles from './users.module.scss'
-import * as axios from 'axios'
 
-class Users extends React.Component {
-  componentDidMount() {
-    //запрос на сервер и получение двнных оттуда и вставкой выбранной страницы и размер кол-ва юзеров на странице
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        //получить всех пользователей с сервера
-        this.props.setUsers(response.data.items)
-        //получить все страницы с сервера
-        this.props.setTotalUsersCount(response.data.totalCount)
-      })
-  }
 
-  //по клику на кнопку
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber)
-    //запрос на сервер и получение двнных оттуда
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items)
-      })
-  }
+let Users = (props) =>{
+  //кол-во юзеров на странице
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
 
-  render() {
-    //кол-во юзеров на странице
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-
-    let pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-      if (pages.length < 10) {
-        pages.push(i)
-      }
+  let pages = []
+  for (let i = 1; i <= pagesCount; i++) {
+    if (pages.length < 10) {
+      pages.push(i)
     }
-
-    return (
-      <div>
-        <div>
-          {pages.map((p) => {
-            return (
-              <span
-                //если текущая страница равна p (странице которая сейчас на экране), то применить стиль (выделить жирным) selectedPage
-                className={this.props.currentPage === p && styles.selectedPage}
-                //p по которой мы итерируемся теперь будет текущей страницей
-                onClick={(e) => {
-                  this.onPageChanged(p)
-                }}
-              >
-                {p}
-              </span>
-            )
-          })}
-        </div>
-        {this.props.users.map((u) => (
-          <div key={u.id}>
-            <span>
-              <div>
-                <img
-                  src={
-                    u.photos.small !== null
-                      ? u.photos.small
-                      : 'https://www.freeiconspng.com/uploads/msn-icon-24.png'
-                  }
-                  alt=""
-                  className={styles.userPhoto}
-                />
-              </div>
-              <div>
-                {u.followed ? (
-                  <button
-                    onClick={() => {
-                      this.props.unfollow(u.id)
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      this.props.follow(u.id)
-                    }}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span>
-              <span>
-                <div>{u.name}</div>
-                <div>{u.status}</div>
-              </span>
-              <span>
-                <div>{'u.location.country'}</div>
-                <div>{'u.location.city'}</div>
-              </span>
-            </span>
-          </div>
-        ))}
-      </div>
-    )
   }
+
+  return (
+    <div>
+      <div>
+        {pages.map((p) => {
+          return (
+            <span
+              //если текущая страница равна p (странице которая сейчас на экране), то применить стиль (выделить жирным) selectedPage
+              className={props.currentPage === p && styles.selectedPage}
+              //p по которой мы итерируемся теперь будет текущей страницей
+              onClick={(e) => {
+                props.onPageChanged(p)
+              }}
+            >
+              {p}
+            </span>
+          )
+        })}
+      </div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={
+                  u.photos.small !== null
+                    ? u.photos.small
+                    : 'https://www.freeiconspng.com/uploads/msn-icon-24.png'
+                }
+                alt=""
+                className={styles.userPhoto}
+              />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id)
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id)
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  )
 }
+
 export default Users
--- src/components/users/UsersContainer.js
+++ src/components/users/UsersContainer.js
@@ -1,6 +1,53 @@
-import Users from './Users'
+import React from 'react'
 import { connect } from 'react-redux'
 import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer'
+import * as axios from 'axios'
+import Users from './Users'
+
+
+//ф-я делает AJAX запрос на сервер 
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    //запрос на сервер и получение двнных оттуда и вставкой выбранной страницы и размер кол-ва юзеров на странице
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        //получить всех пользователей с сервера
+        this.props.setUsers(response.data.items)
+        //получить все страницы с сервера
+        this.props.setTotalUsersCount(response.data.totalCount)
+      })
+  }
+
+  //по клику на кнопку
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    //запрос на сервер и получение двнных оттуда
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items)
+      })
+  }
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+        onPageChanged={this.onPageChanged}
+      />
+    ) 
+  }
+}
 
 //state берется из redux-store, который в свою очередь берется из users-reducer
 //данная ф-я из стейта достает разные данные
@@ -11,6 +58,7 @@ let mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching
   }
 }
 
@@ -36,6 +84,5 @@ let mapDispatchToProps = (dispatch) => {
     },
   }
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
 
-export default UsersContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
