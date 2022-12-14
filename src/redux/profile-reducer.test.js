e901759077df3f2babae9a1702dc502605ebc726
AlbinaAlb <albinakovalevaa@gmail.com>
2022-12-14 12:39:27 +0200

    test

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,13 +1,13 @@
-/* import profileReducer, { addPostActionCreator} from './profile-reducer'
+import profileReducer, { addPostActionCreator } from './profile-reducer'
 
-  let state = {
-    posts: [
-      { id: 1, message: 'Hi, how are you?', likesCount: '15 likes' },
-      { id: 2, message: "It's my first post", likesCount: '20 likes' },
-    ],
-  }
+let state = {
+  posts: [
+    { id: 1, message: 'Hi, how are you?', likesCount: '15 likes' },
+    { id: 2, message: "It's my first post", likesCount: '20 likes' },
+  ],
+}
 
-  //добавить пост и проверить длину
+//добавить пост и проверить длину
 test('length of posts should be incremented', () => {
   //тестовые данные
   let action = addPostActionCreator('it-kam')
@@ -24,4 +24,4 @@ test('message of new post should be correct', () => {
   let newState = profileReducer(state, action)
   //проверяем соответствие текста в третьем элементе
   expect(newState.posts[2].message).toBe('it-kam')
-}) */
+})
